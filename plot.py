5b837bd4119837f2c5fd8bed59d108e651738048
Kyanh56709 <https://github.com/Kyanh56709>
2024-06-05 17:58:56 +0700

    plot linear

--- plot.py
+++ plot.py
@@ -2,15 +2,12 @@ import matplotlib.pyplot as plt
 
 # Your data
 data = [
-    [2000, 0.016, 0.014],
-    [4000, 0.094, 0.088],
-    [6000, 0.171, 0.163],
-    [8000, 0.386, 0.371],
-    [16000, 1.333, 1.314],
-    [20000, 2.061, 2.038],
-    [40000, 7.463, 7.406],
-    [60000, 19.091, 19.011],
-    [10000, 50.123, 50.014]
+    [200000, 1.638, 1.241],
+    [240000, 2.152, 1.653],
+    [280000, 2.376, 1.770],
+    [320000, 2.870, 2.232],
+    [360000, 3.182, 2.656],
+    [400000, 4.013, 3.638]
 ]
 
 y = [row[0] for row in data]
@@ -20,12 +17,12 @@ x2 = [row[2] for row in data]
 # Create the plot
 plt.figure(figsize=(8, 6))  # Adjust figure size if needed
 
-plt.plot(x1, y, label="x1", marker='o', linestyle='-')  # Plot x1 vs. y
-#plt.plot(x2, y, label="x2", marker='s', linestyle='-')  # Plot x2 vs. y
+plt.plot(x1, y, label="T_w_com", marker='o', linestyle='-')  # Plot x1 vs. y
+plt.plot(x2, y, label="T_wo_com", marker='s', linestyle='-')  # Plot x2 vs. y
 
-plt.xlabel("x1 and x2")
-plt.ylabel("y")
-plt.title("Plot of y vs. x1 and x2")
+plt.xlabel("T")
+plt.ylabel("n_samples")
+plt.title("Plot ot vs n_samples")
 plt.legend()
 plt.grid(True)  # Add grid lines
 plt.show()
